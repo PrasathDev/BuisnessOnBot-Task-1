43ffd4525c8098c457e81d587f5e4c1c195835cf
PrasathDev <sharmilaka0921@gmail.com>
2022-05-27 21:55:12 +0530

    first commit

---
+++ api.js
@@ -0,0 +1,30 @@
+const client = require('./connection.js')
+const express = require('express');
+const app = express();
+
+app.listen(3300, ()=>{
+    console.log("Sever is now listening at port 3300");
+})
+
+client.connect();
+
+app.get('/api/branch', (req, res)=>{
+    let query = res.body;
+    client.query(`select * from branches where branch ILike '${req.query.q}%' order by ifsc LIMIT ${req.query.limit} OFFSET ${req.query.offset};`, (err, result)=>{
+        if(!err){
+            res.send({"branches":result.rows});
+        }
+    });
+    client.end;
+})
+
+
+app.get('/api/search', (req, res)=>{
+    let query = res.body;
+    client.query(`select * from branches where branch ILike '%${req.query.q}%' or city ILike '%${req.query.q}%' or ifsc ILike '%${req.query.q}%' or district ILike '%${req.query.q}%' or state ILike '%${req.query.q}%'  order by ifsc LIMIT ${req.query.limit} OFFSET ${req.query.offset};`, (err, result)=>{
+        if(!err){
+            res.send({"branches":result.rows});
+        }
+    });
+    client.end;
+})
