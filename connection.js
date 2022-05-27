4fb6fc5520b48bb16fd6009265203d737e9fe5d2
Prasath RK <59643079+PrasathDev@users.noreply.github.com>
2022-05-27 22:47:45 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg')
 
 const client = new Pool({
- connectionString: “postgres://ycnxyatnixlhqq:e022b70fb0b78de17e6f73a63062b062c04aeb7efb48fd5314c630271b7e2e0c@ec2-3-211-221-185.compute-1.amazonaws.com:5432/dbu5f7h7gsan54”,
+ connectionString: "postgres://ycnxyatnixlhqq:e022b70fb0b78de17e6f73a63062b062c04aeb7efb48fd5314c630271b7e2e0c@ec2-3-211-221-185.compute-1.amazonaws.com:5432/dbu5f7h7gsan54",
  ssl: {
  rejectUnauthorized: false
  }
